2afaf3f5055de510c519cc38346aa999fdf7eaf6
oo7ph <Phillip@core-tech.com>
2013-08-08 21:24:29 -0600

    mend

---
+++ underscore-proto.js
@@ -0,0 +1,53 @@
+/** Installs array, collection, and function methods from UndescoreJS to the Array prototype for more elegant syntax. 
+	@see		http://documentcloud.github.com/underscore
+	@remarks	Includes all array and collection functions except: intersection, union, zip, range, toArray
+*/
+
+/** Returns a new array containing the elements of the given array shifted n spaces to the left, wrapping around the end. */
+var rotate = function(arr, n) {
+	var output = [];
+	var len = arr.length;
+	for(var i=0; i<len; i++) {
+		output.push(index(arr, i+n));
+	}
+	return output;
+};
+
+/** Returns a new function that forwards 'this' as the first parameter to the given function, and thus can be called as instance method (or prototype method ) of the object itself. 
+	@param thisIndex	Forwards 'this' at the given parameter index. Default: 0.
+*/
+var toInstance = function(f, thisIndex) {
+	thisIndex = thisIndex || 0;
+	return function() {
+		var args = Array.prototype.slice.apply(arguments);
+		return f.apply(this, rotate([].concat([this], args), -thisIndex));
+	};
+};
+
+/** Assigns the given list of methods from the host object to the protoObj's prototype after converting them with toInstance. */
+var install = function(protoObj, host, methods, thisIndex) {
+	var len = methods.length;
+	for(var i=0; i<len; i++) {
+
+		// the method can be a string if the hostKey and protoKey are the same ('contains') or an object that maps the host key to the proto key ({repeatString: 'repeat'})
+		var hostKey, protoKey;
+		if(typeof(methods[i]) === 'string') {
+			hostKey = methods[i];
+			protoKey = methods[i];
+		}
+		else {
+			for(var name in methods[i]) {
+				hostKey = name;
+				protoKey = methods[i][name];
+				break;
+			}
+		}
+
+		protoObj.prototype[protoKey] = toInstance(host[hostKey], thisIndex);
+	}
+};
+
+install(Array, _, ["first", "rest", "last", "compact", "flatten", "without", "difference", "uniq", "indexOf", "lastIndexOf", "each", "map", "reduce", "reduceRight", "detect", "select", "reject", "all", "any", "include", "invoke", "pluck", "max", "min", "sortBy", "groupBy", "sortedIndex", "size", "head", "tail", "unique", "forEach", "inject", "foldl", "foldr", "filter", "every", "some", "contains"]);
+install(Function, _, ["bind", "memoize", "delay", "defer", "throttle", "debounce", "once", "wrap", "compose"]);
+install(Function, _, ["after"], -1);
+
